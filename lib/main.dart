3533804db37eee511d890a4d936ca25db931e2a9
xziztt <43674575+xziztt@users.noreply.github.com>
2020-04-30 20:19:05 +0530

    sh: wq: command not found #

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(// containers with no children = as big as possibl. container is a single child widget
+            child: Text("hello there"),
+            color: Colors.black,
+            height: 100,
+            width: 199,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20), //or FromLTRB or EdgeInsets.all or only
+            padding: EdgeInsets.all(10),
+
+          ),
+        ),//widget
       ),
-    ),
-  );
+    );
+  }
 }
